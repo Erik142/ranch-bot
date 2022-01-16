21480ad590521f0b2205d113c585aa0605a1fc2f
Erik Wahlberger <ewahlberger@gmail.com>
2022-01-16 17:02:44 +0100

    Change Bot to hikari lightbulb

--- src/ranchbot/core/bot.py
+++ src/ranchbot/core/bot.py
@@ -4,73 +4,77 @@ from pathlib import Path
 from sys import platform
 from asyncio import AbstractEventLoop
 
-import discord
-from discord.ext import commands
+import hikari
+import hikari.presences
+import lightbulb
 
 from util.log import log
 
+bot = lightbulb.BotApp(token="")
 
-class Bot(commands.Bot):
+
+class Bot(lightbulb.BotApp):
 
     __COGS_BASE_PATH = "commands"
     __COG_FILE_REGEXP = "**/*.py"
 
-    __STATUS = ""
-    __LOGGER = log.getLogger(__name__)
+    __status = ""
+    __plugins = []
+    __logger = log.getLogger(__name__)
 
-    def __init__(self, prefix: str, status: str, eventLoop: AbstractEventLoop = None):
-        intents = discord.Intents.default()
+    def __init__(
+        self, token: str, prefix: str, status: str, eventLoop: AbstractEventLoop = None
+    ):
+        if os.name != "nt":
+            import uvloop
 
-        if eventLoop == None:
-            super().__init__(
-                command_prefix=prefix,
-                help_command=None,
-                intents=intents,
-                loop=eventLoop,
-            )
-        else:
-            intents.members = True
-            super().__init__(
-                command_prefix=prefix,
-                help_command=None,
-                intents=intents,
-            )
-        self.__STATUS = status
-        self.__COGS = self.__getCogs()
+            uvloop.install()
+        super().__init__(token, prefix)
+        self.__status = status
 
-    def __getCogs(self) -> list[str]:
+    #        self.__cogs = self.__getCogs()
+
+    def __get_plugins(self) -> list[str]:
         pathSeparator = "/"
         if platform == "win32":
             pathSeparator = "\\"
         cogs = list[str]()
         scriptPath = os.path.dirname(os.path.realpath(__file__))
-        cogsPath = os.path.join(scriptPath, "../", self.__COGS_BASE_PATH)
+        cogsPath = os.path.join(scriptPath, "../", self.__cogs_BASE_PATH)
 
         for path in Path(cogsPath).glob(self.__COG_FILE_REGEXP):
             pathStr = str(path)
             if "__init__.py" not in pathStr:
                 pathStr = str.removesuffix(pathStr, ".py")
                 pathStr = str.replace(pathStr, ".", "")
-                pathStr = pathStr[pathStr.find(self.__COGS_BASE_PATH) :]
-                self.__LOGGER.debug(pathStr)
+                pathStr = pathStr[pathStr.find(self.__cogs_BASE_PATH) :]
+                self.__logger.debug(pathStr)
                 pathStr = str.removeprefix(pathStr, pathSeparator)
                 pathStr = str.removesuffix(pathStr, pathSeparator)
                 pathStr = str.replace(pathStr, pathSeparator, ".")
                 cogs.append(pathStr)
         return cogs
 
-    def loadCommands(self):
-        for cog in self.__COGS:
+    def load_plugins(self):
+        for plugin in self.__plugins:
             try:
-                self.__LOGGER.info(f"Loading cog {cog}")
-                self.load_extension(cog)
-                self.__LOGGER.info(f"Loaded cog {cog}")
+                self.__logger.info(f"Loading cog {plugin}")
+                self.load_extension(plugin)
+                self.__logger.info(f"Loaded cog {plugin}")
             except Exception as e:
                 exc = "{}: {}".format(type(e).__name__, e)
-                self.__LOGGER.error("Failed to load cog {}\n{}".format(cog, exc))
+                self.__logger.error("Failed to load cog {}\n{}".format(plugin, exc))
 
-    async def on_ready(self):
-        self.__LOGGER.info("Bot is ready!")
-        await self.change_presence(
-            status=discord.Status.online, activity=discord.Game(self.__STATUS)
+    def run(self) -> None:
+        self.event_manager.subscribe(hikari.StartedEvent, self.on_started)        
+        
+        super().run(
+            activity=hikari.Activity(
+                name=self.__status,
+                type=hikari.presences.ActivityType.PLAYING
+            )
         )
+
+    async def on_started(self, event: hikari.StartedEvent):
+        self.__logger.info("Bot is started!")
+        self.load_plugins()
