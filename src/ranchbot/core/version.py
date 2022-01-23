9d325a89808cd30cd703bd477e7f3dbcd3b62697
Erik Wahlberger <ewahlberger@gmail.com>
2022-01-23 23:58:30 +0100

    Cleanup debug messages in version.py

--- src/ranchbot/core/version.py
+++ src/ranchbot/core/version.py
@@ -7,10 +7,7 @@ def get_version() -> str:
     script_path = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(script_path, "../", "../", "../", CONFIG_NAME)
 
-    print(config_path)
-
     config = toml.load(config_path)
-    print(config)
 
     return config['tool']['poetry']['version']
 
